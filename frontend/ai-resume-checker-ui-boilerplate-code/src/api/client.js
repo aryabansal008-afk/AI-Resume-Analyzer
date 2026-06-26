8cca528167fedcacf45c319ae1dd42db0b6fac1b
Arya Bansal <aryabansal008@gnmail.com>
2026-06-26 14:23:05 +0530

    Updated apiClient

--- frontend/ai-resume-checker-ui-boilerplate-code/src/api/client.js
+++ frontend/ai-resume-checker-ui-boilerplate-code/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-  baseURL: "https://ai-resume-analyser-backend-ls59.onrender.com" + "/api",
+  baseURL: "/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
@@ -21,3 +21,5 @@ apiClient.interceptors.response.use(
     });
   }
 );
+
+//baseURL: "https://ai-resume-analyser-backend-ls59.onrender.com" + "/api",
